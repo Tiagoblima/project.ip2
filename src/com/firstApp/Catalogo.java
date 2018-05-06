110c8cebb82778844f4b0696d7ea5975135b890f
Arthur Macedo <arthur.macedo.bandeira@gmail.com>
2018-05-06 10:24:12 -0300

    Adequação da classe Catalogo

--- src/com/firstApp/Catalogo.java
+++ src/com/firstApp/Catalogo.java
@@ -3,5 +3,15 @@ package com.firstApp;
 public class Catalogo {
     private Filme[] filmes;
 
+    public Catalogo(Filme[] filmes) {
+        this.filmes = filmes;
+    }
 
+    public Filme[] getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(Filme[] filmes) {
+        this.filmes = filmes;
+    }
 }
