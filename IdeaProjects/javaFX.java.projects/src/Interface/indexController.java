0291fc8bbb6144f496c80131f6f51c44b241b8b1
Tiagoblima <tiago.blima@ufrpe.br>
2018-05-12 16:20:37 -0300

    Atualiando com a implementação da mudança de tela

--- IdeaProjects/javaFX.java.projects/src/Interface/indexController.java
+++ IdeaProjects/javaFX.java.projects/src/Interface/indexController.java
@@ -1,14 +1,18 @@
 package Interface;
 
 
+
 import javafx.event.ActionEvent;
+
 import javafx.scene.control.*;
 
-public class Controller {
+
+public class indexController {
 
     public TextField loginField;
     public Button btnsingUp;
     public PasswordField passwordField;
+
 
 
     public void login(ActionEvent actionEvent) {
@@ -16,5 +20,9 @@ public class Controller {
     }
 
     public void cadastrar(ActionEvent actionEvent) {
+
+            Main.changeScreen(2);
     }
+
+
 }
