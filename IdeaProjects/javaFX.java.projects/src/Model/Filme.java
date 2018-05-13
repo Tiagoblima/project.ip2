eb26692fa3ca0bbc109884b1120838bc5d141d24
Arthur Macedo <arthur.macedo.bandeira@gmail.com>
2018-05-12 23:23:39 -0300

    Implementação completa e atualizada da classe Filme.

--- IdeaProjects/javaFX.java.projects/src/Model/Filme.java
+++ IdeaProjects/javaFX.java.projects/src/Model/Filme.java
@@ -2,12 +2,23 @@ package Model;
 
 import Interface.Main;
 
+import java.util.Objects;
+
 public class Filme {
 
     private String titulo;
-    private int duracao;
-    private String dataLanc;
+    private int duracaoHora;
+    private int duracaoMinuto;
+    private int anoLanc;
     private String sinopse;
+
+    public Filme(String titulo, int duracaoHora, int duracaoMinuto, int anoLanc, String sinopse) {
+        this.titulo = titulo;
+        this.duracaoHora = duracaoHora;     //SERÁ PRECISO TRATAR ESSA ENTRADA, POIS ELA NÃ PODERÁ SER MAIOR QUE 23
+        this.duracaoMinuto = duracaoMinuto; //SERÁ PRECISO TRATAR ESSA ENTRADA, POIS ELA NÃ PODERÁ SER MAIOR QUE 59
+        this.anoLanc = anoLanc;
+        this.sinopse = sinopse;
+    }
 
     public String getTitulo() {
         return titulo;
@@ -17,20 +28,28 @@ public class Filme {
         this.titulo = titulo;
     }
 
-    public int getDuracao() {
-        return duracao;
+    public int getDuracaoHora() {
+        return duracaoHora;
     }
 
-    public void setDuracao(int duracao) {
-        this.duracao = duracao;
+    public void setDuracaoHora(int duracaoHora) {
+        this.duracaoHora = duracaoHora;
     }
 
-    public String getDataLanc() {
-        return dataLanc;
+    public int getDuracaoMinuto() {
+        return duracaoMinuto;
     }
 
-    public void setDataLanc(String dataLanc) {
-        this.dataLanc = dataLanc;
+    public void setDuracaoMinuto(int duracaoMinuto) {
+        this.duracaoMinuto = duracaoMinuto;
+    }
+
+    public int getAnoLanc() {
+        return anoLanc;
+    }
+
+    public void setAnoLanc(int anoLanc) {
+        this.anoLanc = anoLanc;
     }
 
     public String getSinopse() {
@@ -39,5 +58,36 @@ public class Filme {
 
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+        if (!(o instanceof Filme)) {
+            return false;
+        }
+        Filme filme = (Filme) o;
+
+        return getDuracaoHora() == filme.getDuracaoHora() &&
+                getDuracaoMinuto() == filme.getDuracaoMinuto() &&
+                getAnoLanc() == filme.getAnoLanc() &&
+                Objects.equals(getTitulo(), filme.getTitulo()) &&
+                Objects.equals(getSinopse(), filme.getSinopse());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getTitulo(), getDuracaoHora(), getDuracaoMinuto(), getAnoLanc(), getSinopse());
+    }
+
+    @Override
+    public String toString() {
+        return  "Título: " + titulo + "/n" +
+                "Duração: "+ duracaoHora + ":" + duracaoMinuto + "/n"+
+                "Ano de Lançamento: " + anoLanc + "/n"+
+                "Sinopse: /n" + sinopse;
     }
 }
