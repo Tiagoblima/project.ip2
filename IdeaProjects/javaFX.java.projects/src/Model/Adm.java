f52ab83cd6292d968560cc44006800da1468990e
Tiagoblima <tiago.blima@ufrpe.br>
2018-05-23 21:44:41 -0300

    testando

--- IdeaProjects/javaFX.java.projects/src/Model/Adm.java
+++ IdeaProjects/javaFX.java.projects/src/Model/Adm.java
@@ -4,6 +4,7 @@ public class Adm extends Pessoa {
 
     public Adm(Catalogo catalogo, String nome, int idade, String cpf, String dataAniv, Endereco endereco, String login, String senha) {
         super( nome, idade, cpf,dataAniv, endereco, login, senha);
+        
     }
 
 
