7cf685b96e74ee227b699ae9c20c1db84b0e9268
Tiagoblima <tiago.blima@ufrpe.br>
2018-05-27 08:55:55 -0300

    Realizei algumas mudanças quanto a simplificação do projeto deixando ele o mais simples possível para que eu comece a integração com a parte de interface.

--- src/Model/ClienteComum.java
+++ src/Model/ClienteComum.java
@@ -2,7 +2,7 @@ package Model;
 import java.util.*;
 public class ClienteComum extends Cliente {
     public ClienteComum(int qtdFilmes, double credito, String nome, int idade, String cpf, Date dataAniv, Endereco endereco, String login, String senha) {
-        super(qtdFilmes, credito, nome, idade, cpf, dataAniv, endereco, login, senha);
+        super(qtdFilmes, credito, nome, dataAniv, login, senha);
     }
     //TODO Pensar nas desvantagens de ser um Cliente Comum
 }
