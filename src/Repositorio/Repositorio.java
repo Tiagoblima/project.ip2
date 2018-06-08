f5d5cb7bd9bf81904a628164e918ddd051e4bc9c
Tiagoblima <tiago.blima@ufrpe.br>
2018-06-08 08:59:35 -0300

    Implementação DAO classe hashMap cliente comum

---
+++ src/Repositorio/Repositorio.java
@@ -0,0 +1,74 @@
+package Repositorio;
+
+
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Repositorio <T> implements Serializable{
+
+
+    private T object;
+    private FileOutputStream fout;
+    private FileInputStream fInput;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    public T getHashMap() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+
+
+    public void salvar(String filePath){
+
+        try {
+
+            this.fout = new FileOutputStream(filePath);
+
+            this.oos = new ObjectOutputStream(this.fout);
+
+            this.oos.writeObject(this.object);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não Encontrado");
+            e.printStackTrace();
+        }catch (EOFException  e){
+
+            System.out.println("problema na leitura do arquivo");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public T ler(String filePath){
+
+        try {
+
+            this.fInput = new FileInputStream(filePath);
+            this.ois = new ObjectInputStream(fInput);
+
+            return (T) this.ois.readObject();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não Encontrado");
+            e.printStackTrace();
+        }catch (EOFException  e){
+            System.out.println("problema na leitura do arquivo");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
