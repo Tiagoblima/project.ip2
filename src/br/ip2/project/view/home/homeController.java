998b711ee163a893e3becaab7cd853e166d3e6a4
Tiagoblima <tiago.blima@ufrpe.br>
2018-07-07 20:00:03 -0300

    Adaptação a marcação e atualização da página home

--- src/br/ip2/project/view/home/homeController.java
+++ src/br/ip2/project/view/home/homeController.java
@@ -1,5 +1,8 @@
 package br.ip2.project.view.home;
 
+import br.ip2.project.view.index.indexController;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebEngine;
@@ -10,6 +13,15 @@ import java.awt.event.MouseEvent;
 public class homeController {
 
     public AnchorPane archo;
+    public Pane action;
+    public Pane adventure;
+    public Pane drama;
+    public Pane funny;
+    public Pane family;
+    public Pane nacional;
+    public Image image;
+    public homeController() {
+    }
 
     public void Webview(MouseEvent mouseEvent){
 
@@ -18,9 +30,8 @@ public class homeController {
         WebEngine webEngine = webView.getEngine();
 
         webEngine.load("https://www.imdb.com/");
-
-
         archo.getChildren().addAll(webView);
+
     //    webView.getChildrenUnmodifiable().addAll(webView);
 
 
