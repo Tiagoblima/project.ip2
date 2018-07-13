e56af510e58c43fbf79d67348e27ed9a99cb5d08
Aluno <tiago.blima@ufrpe.br>
2018-07-13 14:52:37 -0300

    Adicição do maven support

---
+++ src/main/java/br/ip2/project/view/index/Controller.java
@@ -0,0 +1,78 @@
+package br.ip2.project.view.index;
+
+import br.ip2.project.controller.ClienteController;
+import br.ip2.project.view.Main;
+import br.ip2.project.view.Scenes;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
+
+
+public class Controller {
+
+    public TextField loginField;
+    public Button btnCadastro;
+    public Button btnEntrar;
+    public PasswordField passField;
+    public javafx.scene.image.ImageView ImageView;
+    public Label lblMsg;
+    public ClienteController controllerCliente = new ClienteController();
+
+
+    public  Image image;
+
+
+    public Controller() {
+
+        image = new Image(Controller.class.getResourceAsStream("../img/action.pane.index.jpg"));
+    }
+
+    public void toLogin(ActionEvent actionEvent) {
+
+
+        String msg = "Acesso liberado";
+
+        boolean acess = true;
+
+        String login = loginField.getText();
+        String senha = passField.getText();
+
+        if(login.isEmpty() || senha.isEmpty()){
+            msg = "Um ou mais campos\n não estão preenchidos";
+            acess = false;
+        }else{
+            try{
+
+                controllerCliente.login(login, senha);
+            }catch (Exception e){
+                msg = "Login ou senha incorretos";
+                acess = false;
+            }
+        }
+
+        if(acess){
+            toHome(actionEvent);
+        }
+
+        lblMsg.setText(msg);
+    }
+
+    public void toForm(ActionEvent actionEvent) {
+
+        Main.changeScreen(Scenes.Form);
+    }
+
+    public void toHome(ActionEvent actionEvent){
+
+        Main.changeScreen(Scenes.Home);
+
+    }
+    public void changeImg(MouseEvent mouseEvent) {
+
+        ImageView.setImage(this.image);
+        ImageView.setVisible(true);
+    }
+}
---
+++ src/main/java/br/ip2/project/view/perfil/Controller.java
@@ -0,0 +1,45 @@
+package br.ip2.project.view.perfil;
+
+import br.ip2.project.controller.ClienteController;
+import br.ip2.project.model.Cliente;
+import br.ip2.project.model.ClienteComum;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public Circle circle;
+
+    public Label tipoCliente;
+    public Label lblFilmes;
+    public Label lblNomeUsuario;
+    public Label lblCreditos;
+    public Button btnMaisPerfil;
+    private ClienteController cc;
+    private HashMap<String, Cliente>  cHashMap;
+    private Cliente cliente;
+
+    public Controller(String login){
+        this.cc = new ClienteController();
+        this.cHashMap =   this.cc.getCCHashMap();
+       this.cliente = this.cHashMap.get(login);
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        this.lblNomeUsuario.setText(this.cliente.getLogin());
+        this.lblFilmes.setText(String.valueOf(this.cliente.getQtdFilmes()));
+        this.lblCreditos.setText(String.valueOf(this.cliente.getCredito()));
+
+        if(this.cliente.getTipoCliente().equals("Comum")){
+
+        }
+    }
+}
