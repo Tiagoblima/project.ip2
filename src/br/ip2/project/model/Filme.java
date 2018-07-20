cc3cb6adced5951f70264c4f6db0230acdb79a3f
Arthur Macedo <arthur.macedo.bandeira@gmail.com>
2018-07-20 14:57:38 -0300

    teste

--- src/br/ip2/project/model/Filme.java
+++ src/br/ip2/project/model/Filme.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Filme implements Serializable {
-
+//commit teste
 
     private String titulo;
     private GeneroFilme genero;
