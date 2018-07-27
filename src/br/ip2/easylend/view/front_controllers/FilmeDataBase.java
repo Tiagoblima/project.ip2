732c2aeef6a999e82e9c80bbfe108968bb568198
Aluno <tiago.blima@ufrpe.br>
2018-07-27 14:36:21 -0300

    modelo sinopse

--- src/br/ip2/easylend/view/front_controllers/FilmeDataBase.java
+++ src/br/ip2/easylend/view/front_controllers/FilmeDataBase.java
@@ -5,6 +5,13 @@ public class FilmeDataBase {
         //  Trailler width: 450 height: 275
         // Filme width: 1382 height 784
 
+        //Modelo sinopse
+//                        "Captain John Miller (Tom Hanks) takes his men behind enemy \n" +
+//        "                \\nlines to find Private James Ryan, whose three brothers have\n" +
+//        "                \\nbeen killed in combat. Surrounded by the brutal realties of war,\n" +
+//        "                \\nwhile searching for Ryan, each man embarks upon a personal journey\n" +
+//        "                \\nand discovers their own strength to triumph over an uncertain future\n" +
+//        "                \\nwith honor, decency and courage.";
 
         public void drama1(){
 
