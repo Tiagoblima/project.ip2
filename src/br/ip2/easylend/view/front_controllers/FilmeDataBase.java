4403546c315b38c44e9f918b16c55cf440b4c3c1
Aluno <tiago.blima@ufrpe.br>
2018-07-27 14:28:27 -0300

    files

--- src/br/ip2/easylend/view/front_controllers/FilmeDataBase.java
+++ src/br/ip2/easylend/view/front_controllers/FilmeDataBase.java
@@ -2,6 +2,9 @@ package br.ip2.easylend.view.front_controllers;
 
 public class FilmeDataBase {
 
+        //  Trailler width: 450 height: 275
+        // Filme width: 1382 height 784
+
 
         public void drama1(){
 
